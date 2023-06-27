52d711c006f69e741b127e9b5bffbe9dd66af9c1
Gangbean <xbox2537@gmail.com>
2023-06-27 12:15:35 +0900

    Feat: implements jwt

---
+++ src/main/java/com/gangbean/stockservice/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.gangbean.stockservice.controller;
+
+import com.gangbean.stockservice.dto.UserDto;
+import com.gangbean.stockservice.entity.User;
+import com.gangbean.stockservice.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
+    }
+
+    @PostMapping("/test-redirect")
+    public void testRedirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/api/user");
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> signup(
+        @Valid @RequestBody UserDto userDto
+    ) {
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<UserDto> getMyUserInfo() {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<UserDto> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username));
+    }
+}
---
+++ src/main/java/com/gangbean/stockservice/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package com.gangbean.stockservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
