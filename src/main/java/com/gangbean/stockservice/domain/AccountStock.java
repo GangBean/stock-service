d6344d11de12ce0c91c7be3c5c86d05161b7ca05
jsh <xbox2537@gmail.com>
2023-07-06 21:03:58 +0900

    Feat: implements AccountStock repository

--- src/main/java/com/gangbean/stockservice/domain/AccountStock.java
+++ src/main/java/com/gangbean/stockservice/domain/AccountStock.java
@@ -1,17 +1,38 @@
 package com.gangbean.stockservice.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class AccountStock {
 
-    private Account account;
-    private Stock stock;
-    private Long balance;
-    private Long averagePrice;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public AccountStock(Account account, Stock stock, Long balance, Long averagePrice) {
+    @ManyToOne
+    private Account account;
+
+    @ManyToOne
+    private Stock stock;
+
+    private Long balance;
+
+    private Long price;
+
+    public AccountStock() {
+    }
+
+    public AccountStock(Account account, Stock stock, Long balance, Long price) {
         this.account = account;
         this.stock = stock;
         this.balance = balance;
-        this.averagePrice = averagePrice;
+        this.price = price;
+    }
+
+    public AccountStock(Long id, Account account, Stock stock, Long balance, Long price) {
+        this(account, stock, balance, price);
+        this.id = id;
     }
 
     public Account account() {
@@ -26,7 +47,24 @@ public class AccountStock {
         return balance;
     }
 
-    public Long averagePrice() {
-        return averagePrice;
+    public Long price() {
+        return price;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountStock that = (AccountStock) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
